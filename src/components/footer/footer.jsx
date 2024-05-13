aab31fc2714026b4d7022c0c8c50179ee3f6f2e4
sohyun <rmsidthgusdl81@gmail.com>
2024-05-13 14:26:15 +0900

    footer change

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,26 +10,22 @@ const Wrapper = styled.div`
     color: gray;
 `;
 const Copytxt = styled.div`
-    transform: translateY(-10px) translateX(207px);
-    width: 190%;
+    transform: translateY(-10px) translateX(170px);
+    width: 100%;
 `;
 const Toptxt = styled.div`
     width: 100%;
-    transform: translateY(-10px) translateX(600px);
-`;
-const Visittxt = styled.div`
-    display: flex;
-    align-items: center;
-    width: 180%;
-    transform: translateY(25px) translateX(-730px);
+    transform: translateY(-10px) translateX(450px);
 `;
 
 const Footer = () => {
     return (
         <Wrapper>
-            <Copytxt><p>Album example is © Bootstrap, but please download and customize it for yourself!</p></Copytxt>
+            <Copytxt>
+                <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
+                <p>New to Bootstrap? <a href="#">Visit the homepage</a> or read our <a href="#">getting started guide</a>.</p>
+            </Copytxt>
             <Toptxt><a href="#">Back to top</a></Toptxt>
-            <Visittxt ><p>New to Bootstrap? <a href="#">Visit the homepage</a> or read our <a href="#">getting started guide</a>.</p></Visittxt>
         </Wrapper>
     );
 };
